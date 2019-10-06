5e6c18b98ce00a85e4a5f5436b48412dda117f6c
solanav <solanav@qq.com>
2019-10-06 20:46:15 +0200

    Added default tcp values for SYN

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ class Forge():
     def __init__(self, interface):
         self.interface = interface
 
+        self.packet = bytearray()
+
         # Ethernet
         self.src_mac = bytearray(6)
         self.dst_mac = bytearray(6)
@@ -29,12 +31,42 @@ class Forge():
         self.ttl     = 0 # Max time to live (255)
         self.dst     = bytearray(4) # Destination IP
 
+        # TCP default values
+        self.src_port = b'\xd0\x62' # 53346
+        self.dst_port = b'\x00\x50' # 80
+        self.seqnum   = b'\x00\x00\x00\x00'
+        self.acknum   = b'\x00\x00\x00\x00' 
+        self.dataoff  = 0b01010000 # Header size 5
+        self.reserved = 0b00000000 # Nop
+        self.ns       = 0b00000000 # No experimental things
+        self.cwr      = 0b00000000 # Not a response
+        self.ece      = 0b00000000 # Idk what ecn is so no
+        self.urg      = 0b00000000 # Not urgent?
+        self.ack      = 0b00000000 # Not an ack
+        self.psh      = 0b00000000 # Don't push?
+        self.rst      = 0b00000000 # Don't reset
+        self.syn      = 0b00000010 # Send a syn
+        self.fin      = 0b00000000 # Not my last packet (hehe) 
+        self.win_size = b'\x00\x00'
+        self.checksum = b'\x00\x00'
+        self.urg_ptr  = b'\x00\x00'
+
     def __ip_to_hex(self, ip):
         hex_ip = []
         for letter in ip.split('.'):
             hex_ip.append(int(letter))
 
         return bytearray(hex_ip)
+
+    def __add(self, value):
+        if type(value) == bytearray:
+            self.packet.extend(value)
+        elif type(value) == int:
+            self.packet.append(value)
+        elif type(value) == bytes:
+            self.packet.extend(bytearray(value))
+        else:
+            print("Error, type not recognized")
 
     def add_eth(self, src_mac, dst_mac):
         if type(src_mac) != bytes or type(dst_mac) != bytes:
@@ -80,34 +112,49 @@ class Forge():
 
     def generate_eth(self):
         header = bytearray()
-        header.extend(self.dst_mac)
-        header.extend(self.src_mac)
-        header.extend(self._type)
+        self.__add(self.dst_mac)
+        self.__add(self.src_mac)
+        self.__add(self._type)
 
         return header
 
     def generate_ipv4(self):
         header = bytearray()
-        header.append(self.version | self.ihl)
-        header.append(self.dscp & 0x11111100 | self.ecn & 0x11)
-        header.extend(self.length)
-        header.extend(self._id)
-        header.append(self.flags >> 8 | self.foff >> 8) # parte de arriba de la palabra
-        header.append(self.flags & 0b11111111 | self.foff & 0b11111111) # parte de abajo
-        header.append(self.ttl)
-        header.extend(self.pro)
-        header.extend(self.check)
-        header.extend(self.src)
-        header.extend(self.dst)
+        self.__add(self.version | self.ihl)
+        self.__add(self.dscp & 0x11111100 | self.ecn & 0x11)
+        self.__add(self.length)
+        self.__add(self._id)
+        self.__add(self.flags >> 8 | self.foff >> 8) # parte de arriba de la palabra
+        self.__add(self.flags & 0b11111111 | self.foff & 0b11111111) # parte de abajo
+        self.__add(self.ttl)
+        self.__add(self.pro)
+        self.__add(self.check)
+        self.__add(self.src)
+        self.__add(self.dst)
+
+        return header
+
+    def generate_syn(self):
+        header = bytearray()
+
+        self.__add(self.src_port)
+        self.__add(self.dst_port)
+        self.__add(self.seqnum)
+        self.__add(self.acknum)
+        self.__add(self.dataoff | self.reserved | self.ns)
+        self.__add(self.cwr | self.ece | self.urg | self.ack | self.psh | self.rst | self.syn | self.fin)
+        self.__add(self.win_size)
+        self.__add(self.checksum)
+        self.__add(self.urg_ptr)
 
         return header
 
     def generate(self):
-        packet = bytearray()
-        packet.extend(self.generate_eth())
-        packet.extend(self.generate_ipv4())
+        self.__add(self.generate_eth())
+        self.__add(self.generate_ipv4())
+        self.__add(self.generate_syn())
 
-        return packet
+        return self.packet
 
     def __str__(self):
         _str = []
